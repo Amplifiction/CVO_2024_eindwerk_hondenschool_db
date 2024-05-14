07221b93c2b366922527bb7275937b67dee1290e
Amplifiction <bart.stevens9@gmail.com>
2024-05-14 15:59:47 +0200

    contactmodal af (links naar externe sites idem, in vorige commit)

--- resources/js/Components/ModalContact.jsx
+++ resources/js/Components/ModalContact.jsx
@@ -1,25 +1,35 @@
 import Modal from "./Modal";
 
 export default function ModalContact ({user, close}) {
-    <Modal
-        close={close}
-    >
-        <div className="flex-row just-end width-100pc">
-            <button
-                onClick={close}
-                className="btn-accent"
-            ><i className="fa-solid fa-xmark"></i></button>
-        </div>
-        <h3 className="m-y-1">
-            {user.first_name} {user.last_name}
-        </h3>
-        <p>Email: <a href={`mailto:${user.email}`}>{user.email}</a></p>
-        {user.cellphone &&
-            <p>GSM: <a href={`tel:${user.cellphone}`}>{user.cellphone}</a></p>
-        }
-        {user.phone &&
-            <p>Telefoon: <a href={`tel:${user.phone}`}>{user.phone}</a></p>
-        }
-    </Modal>
-
+    return (
+        <Modal
+            close={close}
+        >
+            <div className="flex-row just-end width-100pc">
+                <button
+                    onClick={close}
+                    className="btn-accent"
+                ><i className="fa-solid fa-xmark"></i></button>
+            </div>
+            <h3 className="m-y-1">
+                {user.first_name} {user.last_name}
+            </h3>
+            <div>
+                <i className="fa-solid fa-envelope"></i>{' '}
+                <a href={`mailto:${user.email}`}>{user.email}</a>
+            </div>
+            {user.cellphone &&
+                <div>
+                    <i className="fa-solid fa-mobile-screen-button"></i>{' '}
+                    <a href={`tel:${user.cellphone}`}>{user.cellphone}</a>
+                </div>
+            }
+            {user.phone &&
+                <div>
+                    <i className="fa-solid fa-square-phone"></i>{' '}
+                    <a href={`tel:${user.phone}`}>{user.phone}</a>
+                </div>
+            }
+        </Modal>
+    )
 }
--- resources/js/Pages/DashboardAdmin.jsx
+++ resources/js/Pages/DashboardAdmin.jsx
@@ -1,7 +1,7 @@
 import { usePage, Link } from "@inertiajs/react";
 import { useState } from "react"
 import IndexRequestedMs from "../Components/Memberships/IndexRequestedMs";
-import Modal from "../Components/Modal"
+import ModalContact from "../Components/ModalContact"
 
 export default function DashboardAdmin ({requestedMs, statuses}) {
     const { auth } = usePage().props
@@ -34,26 +34,10 @@ export default function DashboardAdmin ({requestedMs, statuses}) {
                 contactModalEvent={handleShowContactModal}
             />
             {showContactModal &&
-                <Modal
+                <ModalContact
+                    user={contactModalUser}
                     close={() => setShowContactModal(false)}
-                >
-                    <div className="flex-row just-end width-100pc">
-                        <button
-                            onClick={() => setShowContactModal(false)}
-                            className="btn-accent"
-                        ><i className="fa-solid fa-xmark"></i></button>
-                    </div>
-                    <h3 className="m-y-1">
-                        {contactModalUser.first_name} {contactModalUser.last_name}
-                    </h3>
-                    <p>Email: <a href={`mailto:${contactModalUser.email}`}>{contactModalUser.email}</a></p>
-                    {contactModalUser.cellphone &&
-                        <p>GSM: <a href={`tel:${contactModalUser.cellphone}`}>{contactModalUser.cellphone}</a></p>
-                    }
-                    {contactModalUser.phone &&
-                        <p>Telefoon: <a href={`tel:${contactModalUser.phone}`}>{contactModalUser.phone}</a></p>
-                    }
-                </Modal>
+                />
             }
         </div>
     )

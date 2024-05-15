40d2fcffd88733ffffd5d216c6b86973f95958fd
Bart Stevens <bart.stevens9@gmail.com>
2024-05-15 12:32:27 +0200

    indexAll layout update

--- resources/js/Components/Memberships/IndexAllMs.jsx
+++ resources/js/Components/Memberships/IndexAllMs.jsx
@@ -112,6 +112,7 @@ export default function IndexAllMs ({allMs, contactModalEvent}) {
                         />
                     </div>
                     <div className="xs-col-6 l-col-3">
+                        <div className="fs-90 flex-row just-center align-center m-xy-1">datumformaat jjjj-(m)m-dd</div>
                         <FilterInput
                             value={statusFilter}
                             onChange={(e) => setStatusFilter(e.target.value)}

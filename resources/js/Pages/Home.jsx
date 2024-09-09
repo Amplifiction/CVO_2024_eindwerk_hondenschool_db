6c0db5d706fe032a040fa1890a4083c9052ee025
Amplifiction <bart.stevens9@gmail.com>
2024-09-09 20:49:23 +0200

    Demoversie div verder uitgewerkt

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -38,8 +38,9 @@ export default function Home ({postal_codes, sexes}) {
                         <li>en zich met een (gedeelde)  hond inschrijven voor één of meerdere disciplines.</li>
                     </ul>
                     <p><span className="fw-bold">Beheerders</span> kunnen bovendien aanvragen goed- of afkeuren en een filterbaar overzicht van alle lidmaatschappen raadplegen.</p>
+                    <p className="m-y-05">Gebruikers worden via email op de hoogte gehouden van belangrijke gebeurtenissen.</p>
                 <h2 className="m-tb fc12">Inloggen</h2>
-                <p>Je kan een eigen account aanmaken, of één van de volgende accounts gebruiken:</p>
+                <p>Je kan registreren, of één van de volgende accounts gebruiken.</p>
                 <p>Zelf aangemaakte accounts zullen steeds de gebruikersrol hebben.</p>
                 <ul className="m-y-1 m-l-2">
                     <li><span className="fw-bold">bart-stevens@outlook.com</span> (Jerom, gebruiker, incl honden en lidmaatschappen)</li>
@@ -49,7 +50,7 @@ export default function Home ({postal_codes, sexes}) {
                 </ul>
                 <p>Wachtwoord van deze accounts is steeds 123456789.</p>
                 <h2 className="m-tb fc12">Under the hood</h2>
-                <p className="m-y-05 ff">
+                <p className="m-y-05">
                     Benieuwd naar de uitdagingen die deze database tackelt, de gebruikte technologie en andere weetjes? Lees erover in&nbsp;
                     <a
                         href= "https://www.canva.com/design/DAGFUjzk2HM/q9qb1Llloskm9wKOAh0H0Q/view?utm_content=DAGFUjzk2HM&utm_campaign=designshare&utm_medium=link&utm_source=editor"
@@ -59,6 +60,14 @@ export default function Home ({postal_codes, sexes}) {
                 <p  className="m-y-05">
                     In het kader van deze demoversie werden bepaalde restricties opgeheven, zoals die mbt email verificatie.
                 </p>
+                <div className="flex-row just-center">
+                    <div className="flex-row just-between fc8 fs-300 min-width-330px">
+                        <i class="fa-brands fa-laravel"></i>
+                        <i class="fa-brands fa-react"></i>
+                        <i class="fa-brands fa-sass"></i>
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                </div>
             </div>
         </div>
     )

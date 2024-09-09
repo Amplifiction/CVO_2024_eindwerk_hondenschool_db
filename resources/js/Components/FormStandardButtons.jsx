8db6e7966186a4d60479c0ad74cc954bafd22b4f
Amplifiction <bart.stevens9@gmail.com>
2024-09-09 18:46:41 +0200

    basePath toegevoegd aan 'annuleren' knop

--- resources/js/Components/FormStandardButtons.jsx
+++ resources/js/Components/FormStandardButtons.jsx
@@ -1,13 +1,16 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 export default function FormStandardButtons ({
     title,
     processing,
 }) {
+
+    const { basePath } = usePage().props
+
     return (
         <div className="flex-row m-y-1">
             <input disabled={processing} type="submit" value={title} className="btn-green" />
-            <Link href="/" as="button" className="btn-gray">Annuleren</Link>
+            <Link href={`${basePath}/`} as="button" className="btn-gray">Annuleren</Link>
         </div>
     )
 }
